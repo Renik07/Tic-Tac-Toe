448e6b5a99d82ea12ee398533a1751afe2e047a4
Renat <restspread@gmail.com>
2021-03-07 19:37:18 +0300

    add current move

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ class App extends React.Component {
 			squares : Array(9).fill(null),
 			count: 0,
 			winX : 0,
-			winY : 0
+			winY : 0,
+			currentMove: 'X'
 		}
 		this.winnerLine = [
 			[0, 1, 2],
@@ -32,7 +33,7 @@ class App extends React.Component {
 			if (this.state.squares[currentLine[0]] === clickCell &&
 				this.state.squares[currentLine[1]] === clickCell &&
 				this.state.squares[currentLine[2]] === clickCell) {
-					alert(clickCell + ' win');
+					alert(clickCell + ' победил');
 
 					if (this.state.count % 2 === 0) {
 						this.setState({winX : this.state.winX + 1})
@@ -43,7 +44,8 @@ class App extends React.Component {
 					setTimeout(() => {
 						this.setState({
 							squares : Array(9).fill(null),
-							count: 0})
+							count: 0,
+							currentMove: 'X'})
 					}, 1000)
 			}
 		}
@@ -63,9 +65,9 @@ class App extends React.Component {
 		}
 		
 		if (this.state.count % 2 === 0) {
-			event.target.style.color = 'rgb(9, 117, 241)';
+			this.setState({currentMove: 'О'});
 		} else {
-			event.target.style.color = 'red';
+			this.setState({currentMove: 'Х'});
 		}
 		// проверка выигрышного состояния
 		this.isWinner(currentSquares[cell]);
@@ -75,11 +77,13 @@ class App extends React.Component {
 			squares : Array(9).fill(null),
 			count: 0,
 			winX : 0,
-			winY : 0})
+			winY : 0,
+			currentMove: 'X'})
 	}
 	render () {
 		return (
 			<div className="wrapper">
+				<div className="current-move">Сейчас ходит: {this.state.currentMove}</div>
 				<div className="ttt">
 					<div className="ttt-grid" onClick={this.clickHandler} data="0">{this.state.squares[0]}</div>
 					<div className="ttt-grid" onClick={this.clickHandler} data="1">{this.state.squares[1]}</div>
